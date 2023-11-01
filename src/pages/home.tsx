a45be420df65418c3b87f82deaa831ceca1fd5db
NadeebaAtiqui <nadeebaatiqui@gmail.com>
2023-11-01 16:13:46 -0500

    fixing margins

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,98 +1,24 @@
-
-// import React from 'react';
-// import Link from 'next/link';
-
-// const Home: React.FC = () => {
-//   return (
-//     <div>
-//       <header>
-//         <h1>Welcome to Your Lab.ms Signed in Page</h1>
-//       </header>
-//       <main>
-//         <p>This is the content of your signed in page.</p>
-//         <div>
-//           <button>
-//             <Link href="/help">Help</Link>
-//           </button>
-//           <button>
-//             <Link href="/reserve-machine">Reserve a Machine</Link>
-//           </button>
-//           <button>
-//             <Link href="/tour">Tour</Link>
-//           </button>
-//           <button>
-//             <Link href="/registration">Registration</Link>
-//           </button>
-//           <button>
-//             <Link href="/signout">Sign Out</Link>
-//           </button>
-//         </div>
-//       </main>
-//       <footer>
-//         &copy; {new Date().getFullYear()} Your Website
-//       </footer>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React from "react";
 import {Button} from "@nextui-org/react";
-import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
+//import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
 
-import styles from '@/styles/Home.module.css';
+// import styles from '@/styles/Home.module.css';
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
-
-
+import Image from 'next/image';
 
 export default function App() {
-  const list = [
-    {
-      title: "Reserve a machine",
-      img: "<images /barn-images-t5YUoHW6zRo-unsplash.jpg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
-      price: "$12.20",
-    },
-  ];
   return (
     <div>
       <Navbar isBordered>
       <NavbarBrand>
+        <p>  <Image
+                src="/Screenshot 2023-11-01 153213.png"
+                alt="Vercel Logo"
+                // className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              /></p>
         <p className="font-bold text-inherit">TheLAB.ms</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
@@ -117,7 +43,7 @@ export default function App() {
           <Link href="#">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          
+
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
@@ -130,7 +56,9 @@ export default function App() {
           <header>
             <h1>Welcome to Your Lab.ms Signed in Page</h1>
           </header>
-          <main  className={`${styles.main}`}>
+          <main  
+          // className={`${styles.main}`}
+          >
             <p>This is the content of your signed in page.</p>
     {/* <div className="flex flex-wrap gap-4 items-center justify-center h-screen">
       {list.map((item, index) => (
@@ -152,21 +80,26 @@ export default function App() {
         </Card>
       ))}
     </div> */}
-            
-    <div className="flex flex-wrap gap-4 items-center justify-center h-screen">
-    <div className="flex-grow">
+
+    <div className="flex gap-4 items-center justify-center py-15">
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="lg" color ="primary">
         Default
       </Button>
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="lg" color="primary">
         Tour
       </Button>  
+      </div>
+      <div className="flex gap-4 items-center justify-center">
+
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="lg" color="primary">
         Reserve a Machine
       </Button>  
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="lg" color="primary">
         Help
-      </Button>  
+      </Button>
+      </div>  
+      <div className="flex gap-4 items-center justify-center">
+
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="lg" color="primary">
         Events 
       </Button>  
@@ -174,12 +107,8 @@ export default function App() {
         Sign Out
       </Button>  
       </div>
-      </div>
-      
+
 
       </main>
     </div>
-
-  );
-}
-
+  )};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import {nextui} from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
+  // prefix : "tw-",
   content: [
     // ...
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
