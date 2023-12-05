b91268c21b51096d0e1ce281eef5ef83fadf50aa
NadeebaAtiqui <nadeebaatiqui@gmail.com>
2023-12-04 21:01:18 -0600

    routing done for tour/reserve_machine/events/unsigned/signed pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,10 +114,12 @@
 // }
 
 import React from "react";
+import Navbar from '../components/navbar';
 
 export default function App() {
   return (
     <div>
+        <Navbar/>
       <iframe
         src="https://docs.google.com/presentation/d/e/2PACX-1vQIVSUaGvNw2cjEoAhuo71u1K2aGYYKePMBgSHbxbl-NnAPxjAF3IzhrK4p0J0Qgd_A6otsJ79-lG_6/embed?start=true&loop=true&delayms=3000"
         width="1600"
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,35 +1,42 @@
 import React from "react";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import Navbar from '../components/navbar';
+import { useRouter } from 'next/router';
+
 
 //import theme from '../themes/theme'; // Adjust the path accordingly
 //import '../styles/globals.css'; // Your global styles
 
 
 export default function App() {
+    const router = useRouter();
     const list1 = [
         {
             title: "ABOUT US",
             // img: "https://nextui.org/images/fruit-1.jpeg",
             img: "/images/about.jpg",
+            onPress: () => router.push('/about_us'),
             //price: "$5.50",
         },
         {
             title: "EVENTS",
             //img: "https://nextui.org/images/fruit-2.jpeg",
             img: "/images/calender.jpg",
+            onPress: () => router.push('/reservation'),
            // price: "$3.00",
         },
         {
             title: "RESERVE MACHINE",
            // img: "https://nextui.org/images/fruit-3.jpeg",
            img: "/images/3D printer.jpg",
+           onPress: () => router.push('/reserve_machine'),
             //price: "$10.00",
         },
         {
             title: "TICKET",
            // img: "https://nextui.org/images/fruit-4.jpeg",
            img: "/images/ticket.jpg",
+           onPress: () => router.push('/ticket'),
             //price: "$5.30",
         },
         
@@ -38,24 +45,28 @@ export default function App() {
         title: "DONATE TO MAKERSPACE",
         //img: "https://nextui.org/images/fruit-5.jpeg",
         img: "/images/donate.jpg",
+        onPress: () => router.push('/ticket'),
        // price: "$15.70",
     },
     {
         title: "PAYMENT OPTIONS",
         // img: "https://nextui.org/images/fruit-6.jpeg",
         img: "/images/payment.jpg",
+        onPress: () => router.push('/ticket'),
        // price: "$8.00",
     },
     {
         title: "REPORT AN EMERGENCY",
         // img: "https://nextui.org/images/fruit-7.jpeg",
         img: "/images/emergency.jpg",
+        onPress: () => router.push('/ticket'),
        // price: "$7.50",
     },
     {
         title: "REQUEST A TOUR",
         // img: "https://nextui.org/images/fruit-8.jpeg",
         img: "/images/tour.jpg",
+        onPress: () => router.push('/tour'),
        // price: "$12.20",
     },]
     return (
@@ -68,7 +79,7 @@ export default function App() {
         <div className="w-full flex flex-grow gap-4  p-5">
          
             {list1.map((item, index) => (
-                <Card className="" shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+                <Card className="" shadow="sm" key={index} isPressable onPress={item.onPress}>
                     <CardBody className="overflow-visible p-0">
                         <Image
                             shadow="sm"
@@ -89,7 +100,7 @@ export default function App() {
         </div>
         <div className=" w-full flex flex-grow gap-4  p-5">
             {list2.map((item, index) => (
-                <Card className="" shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+                <Card className="" shadow="sm" key={index} isPressable onPress={item.onPress}>
                     <CardBody className="overflow-visible p-0">
                         <Image
                             shadow="sm"
