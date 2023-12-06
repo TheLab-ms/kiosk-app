6b459d5b1f5ff10ec4e47f110b6b4d7e11b14820
Cameron Steele <cam.steeleis@gmail.com>
2023-12-06 08:18:46 -0600

    Linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,17 +114,17 @@
 // }
 
 import React from "react";
-import Navbar from '../components/navbar';
+import { Navbar } from '../components/navbar';
 
 export default function App() {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
       <iframe
         src="https://docs.google.com/presentation/d/e/2PACX-1vQIVSUaGvNw2cjEoAhuo71u1K2aGYYKePMBgSHbxbl-NnAPxjAF3IzhrK4p0J0Qgd_A6otsJ79-lG_6/embed?start=true&loop=true&delayms=3000"
         width="1600"
         height="750"
-        //allowFullScreen // Allow full screen is a boolean attribute
+      //allowFullScreen // Allow full screen is a boolean attribute
       />
     </div>
   );
