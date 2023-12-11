de69a4d3b52a8a95b9a7ae527d6d2224a2d3b97a
SriNethala317 <srinethala123@gmail.com>
2023-12-11 12:11:44 -0600

    took out locked signed in page functionality since it wasn't working

--- src/midware.ts
+++ src/midware.ts
@@ -1,20 +1,20 @@
-import { withAuth } from 'next-auth/middleware';
+// import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
-	callbacks: {
-		authorized({ req, token }) {
-			if (
-				req.nextUrl.pathname === '/test'
-			) {
-				console.log('Admitting because the user is an approver');
-				return true;
-			}
-			console.log('No match');
-			return false;
-		},
-	},
-});
+// export default withAuth({
+// 	callbacks: {
+// 		authorized({ req, token }) {
+// 			if (
+// 				req.nextUrl.pathname === '/test'
+// 			) {
+// 				console.log('Admitting because the user is an approver');
+// 				return true;
+// 			}
+// 			console.log('No match');
+// 			return false;
+// 		},
+// 	},
+// });
 
-export const config = {
-	matcher: ['/test'],
-};
+// export const config = {
+// 	matcher: ['/test'],
+// };
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
@@ -6,22 +6,22 @@ import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { useRouter } from 'next/router';
 import DefaultLayout from "@/layouts/default";
 
-export default async function App(
-    req: NextApiRequest,
-	res: NextApiResponse
+export default function App(
+    // req: NextApiRequest,
+	// res: NextApiResponse
 ) {
     
-	if (!req.method || (req.method !== 'POST' && req.method !== 'DELETE')) {
-		res.status(405).json({ message: 'Method not allowed' });
-		return;
-	}
+	// if (!req.method || (req.method !== 'POST' && req.method !== 'DELETE')) {
+	// 	res.status(405).json({ message: 'Method not allowed' });
+	// 	return;
+	// }
 
-	const session = await getServerSession(req, res, authOptions);
-	if (!session) {
-		alert("2nd condition");
-		res.status(401).json({ message: 'Unauthorized' });
-		return;
-	}
+	// const session = await getServerSession(req, res, authOptions);
+	// if (!session) {
+	// 	alert("2nd condition");
+	// 	res.status(401).json({ message: 'Unauthorized' });
+	// 	return;
+	// }
     const router = useRouter();
     const list1 = [
         {
